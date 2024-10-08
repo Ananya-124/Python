f98014a502eacad2e2a33fb0caa28ada7351caad
Ananya <ananyaananyaa124@gmail.com>
2024-10-08 21:24:58 +0530

    n

--- first.py
+++ first.py
@@ -1,8 +1,10 @@
-def celsius() :
-    a=int(input("enter a number"))
-    f=(a*9/5) + 32
-    print(f)    
-    
-celsius()
-
+def isprime():
+    num=int(input("enter a number"))
+    if num<2:
+        return False
+    for i in range(2, num+1):
+        if num%i==0:
+            return False
+        else:
+            return True
 
