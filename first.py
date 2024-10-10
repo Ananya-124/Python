890facc59166c7db5e2a9597c2ff2c7dbbc556cc
Ananya <ananyaananyaa124@gmail.com>
2024-10-10 16:32:32 +0530

    new

--- first.py
+++ first.py
@@ -1,15 +1,8 @@
 import random
-def moveright(right):
-    count=1
-    dog=2
+def lucky():
+    name=input("what is your name")
+    dob=int(input("ente only birth date and month not year "))
+    rad=(random.randrange(0,dob))
     
-    if dog < count:
-        print("🤠😊😊😊")
-    else:
-        print("📖📖📖📖🐘🐘🐴🐴🐴")
-right=(random.randrange(0,11))
-print(right)
-moveright(right)
-
-        
-
+    print( name," your lucky number is" , rad )
+lucky()
