a6957943a3601d7af6cdc0b725e32d541f2e3ead
Ananya <ananyaananyaa124@gmail.com>
2024-10-10 16:58:26 +0530

    new

--- first.py
+++ first.py
@@ -1,14 +1,7 @@
+import string
 import random
-def lucky():
-    name=input("what is your name")
-    dob=int(input("ente only birth date "))
-    rad=(random.randrange(0,dob))
-    length=len(name)
-    
-    print( name," your lucky number is" , (length + rad + 1) , "😊😊" )
-lucky()
-def letter():
-    name=input("enter your name to get your future name")
-    ran=(random.randrange('a','z'))
-    print(ran)
-letter()
+def rand():
+    a=''.join(random.choices(string.ascii_uppercase, string.punctuation,k=2))
+   
+    print(a)
+rand()
