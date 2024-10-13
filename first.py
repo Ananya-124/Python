6362dee2bd681729f001b8282e6979b71a728a6d
Ananya <ananyaananyaa124@gmail.com>
2024-10-13 20:19:37 +0530

    new

--- first.py
+++ first.py
@@ -1,12 +1,7 @@
-import numpy as np
-import matplotlib.pyplot as plt
-x=np.array([20,40])
-y=np.array([10,30])
-font1 = {'family':'serif','color':'blue','size':20}
-font2 = {'family':'serif','color':'darkred','size':15}
-plt.title("HEALTH",font1)
-plt.plot(x,y,'o',linestyle="dotted")
-plt.xlabel("fruits",font1)
-plt.ylabel("price",font2)
-
-plt.show()
+import random
+from collections import Counter
+someWords = '''apple banana mango strawberry 
+orange grape pineapple apricot lemon coconut watermelon 
+cherry papaya berry peach lychee muskmelon'''
+someWords = someWords.split()
+word = random.choice(someWords)
