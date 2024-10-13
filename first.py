71b4effd164f3782d47d1ee6718555a7d64c9073
Ananya <ananyaananyaa124@gmail.com>
2024-10-13 18:45:42 +0530

    numpy

--- first.py
+++ first.py
@@ -1,12 +1,7 @@
-import random
- 
-def lucky():
-    a=input("name")
-    a[0].upper() + a[1:]
-    b=int(input("enter only date or month of your birthday"))
-    c=(random.randrange(0,b))
-    d=c+1
-    print("Hello", a, "Your lucky number is:",d)
-    print(a)
-   
-lucky()
+import numpy as np
+speed = [20,30,40,50,60,70]
+age = [1,2,3,4,5,5,6]
+sd = np.std(speed)
+print(int(sd))
+print(int(np.var(age)))
+print(np.percentile(speed,70))
