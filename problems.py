acf967dd2d55c6ff12441d30be2bc574d9348f54
Ananya <ananyaananyaa124@gmail.com>
2024-10-21 17:31:19 +0530

    problem solved

--- problems.py
+++ problems.py
@@ -1,11 +1,6 @@
-def is_palindrome(s):
-    a = len(s)//2
-    name = s
-    b = name.slice(0,a)
-    print(b)
-def is_symm(s):
-    name =s
-    c = name.split(len(s)//2)
-    print(c)
-is_palindrome("ananya")
-is_symm("lathaa")
+string = "ananya is good girl"
+sp = string.split()[::-1]
+l=[]
+for i in sp:
+    l.append(i)
+print(" ".join(l))
