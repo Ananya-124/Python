bbef20ad20228c620e714b65df559af010a86273
Ananya <ananyaananyaa124@gmail.com>
2024-10-21 17:17:00 +0530

    new

---
+++ problems.py
@@ -0,0 +1,11 @@
+def is_palindrome(s):
+    a = len(s)//2
+    name = s
+    b = name.slice(0,a)
+    print(b)
+def is_symm(s):
+    name =s
+    c = name.split(len(s)//2)
+    print(c)
+is_palindrome("ananya")
+is_symm("lathaa")
