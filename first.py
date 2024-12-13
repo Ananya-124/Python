c45579e989356f6826201e6465e2475a56c874c9
Ananya <ananyaananyaa124@gmail.com>
2024-12-13 19:27:59 +0530

    Ananya 🍉

--- first.py
+++
@@ -1,7 +0,0 @@
-import random
-from collections import Counter
-someWords = '''apple banana mango strawberry 
-orange grape pineapple apricot lemon coconut watermelon 
-cherry papaya berry peach lychee muskmelon'''
-someWords = someWords.split()
-word = random.choice(someWords)
--- problems.py
+++
@@ -1,5 +0,0 @@
-Tuple1 = ("A", 1, "B", 2, "C", 3)
-Tuple2 = ("Geek1", "Raju", "Geek2", "Nikhil", "Geek3", "Deepanshu")
-Tuple3 = ((1, "Lion"), ( 2, "Tiger"), (3, "Fox"), (4, "Wolf"))
-print(Tuple1.__sizeof__())
-print(len(Tuple2))
